eb3893291db0f72e486fa760c1f9dd47b369be1f
Wazha Louis <37450077+WazhaLouis@users.noreply.github.com>
2024-05-12 17:38:47 +0200

    Add files via upload
    This a basic react-native button click counter application made with javascript es6

--- App.js
+++ App.js
@@ -1,52 +1,27 @@
-import { useState } from "react";
-import { View, Text,Image, ScrollView, Button, Modal,StatusBar } from "react-native";
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet,StatusBar} from 'react-native';
 
-const propic = require ("./assets/man.png");
-const backfoto = require ("./assets/adaptive-icon.png");
+export default function App(){
+  const [count, setCount] = useState(0);
 
-
-export default function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false)
   return (
-  <View style={{flex: 1, backgroundColor:"lightgreen", padding: 30 }}>
+    <View style={{ flex :1 , backgroundColor:"lightgreen",padding:50}} >
+      <StatusBar backgroundColor="lightblue" barStyle="light-content"/>
 
-        <ScrollView>
+       <Text>You clicked {count} times</Text>
 
-         <Image  source={propic } style={{ width: 300, height:300 }} /> 
+      <Button
+        onPress={() => setCount (count + 1)}
+        title="Click me!"
+      />
 
-         <Text>Yeah Louis we're doing it </Text>
+      <View style={{ flex :1,padding :20}} >
 
-         <Button title="Button"/>
-
+         <Button title="Reset" onPress={()=> setCount(0) } />
          
-    
-         <View style={{flex: 1, backgroundColor:"lightgreen",padding: 5 }}> 
-            <Image  source={propic } style={{ width: 300, height:300,}} /> 
-
-         <Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris malesuada ultrices feugiat. Aliquam nec varius nisl. Phasellus dapibus euismod neque, vitae dictum elit vestibulum vel. Phasellus tempor lacus sit amet lectus interdum dictum. Donec efficitur, purus quis pellentesque aliquet, massa dolor porta lectus, ut tempus leo erat nec nibh. Cras venenatis mi non diam sollicitudin tincidunt. Sed placerat ornare sodales. Integer at dignissim leo. Sed euismod, felis eget scelerisque facilisis, urna nulla tristique arcu, sit amet pellentesque ligula lacus nec nisl. Ut mauris leo, tempus non auctor eget, eleifend vel risus. Etiam tempus porta magna sit amet ultrices. Etiam in mattis sem, et placerat libero. Praesent non risus vel risus pellentesque blandit ut eu felis. Pellentesque imperdiet fermentum varius. </Text>
-
-         < Button title="Press for modal" 
-         onPress={()=> setIsModalVisible(true)} color="midnightblue" />
-
-         </View>
-
-
-        </ScrollView>
-
- 
-           <Modal visible={isModalVisible}
-              onRequestClose={()=> setIsModalVisible(false)}
-              animationType="fade">
-                 
-
-             <View style={{ flex : 1, backgroundColor:"midnightblue", padding: 60 }}> 
-               <Text>Modal screen </Text>
-               <Button title="close modal" color= "blue" 
-                onPress={()=> setIsModalVisible(false)}/>
-             </View>
+      </View>
      
-              <StatusBar barStyle="light-content" backgroundColor="midnightblue"></StatusBar>
-           </Modal>
-  </View>);
-  
-}
+    </View>
+  );
+};
+
